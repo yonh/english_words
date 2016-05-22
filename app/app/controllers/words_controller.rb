0c0d9f3289270c7fe7ed431f5b3f4c8a4d1265d7
yonh <azssjli@163.com>
2016-05-22 17:14:16 +0800

    word CRUD

--- app/app/controllers/words_controller.rb
+++ app/app/controllers/words_controller.rb
@@ -1,2 +1,42 @@
 class WordsController < ApplicationController
+	def index
+		@words = Word.all
+	end
+	def new
+		  @word = Word.new
+	end
+	def create
+		@word = Word.new(word_params)
+		 
+		if @word.save
+			redirect_to @word
+		else
+			render 'new'
+		end
+	end
+	def show
+		@word = Word.find(params[:id])
+	end
+	def edit
+		@word = Word.find(params[:id])
+	end
+	def update
+  		@word = Word.find(params[:id])
+ 
+  		if @word.update(word_params)
+    		redirect_to @word
+  		else
+    		render 'edit'
+  		end
+	end
+	def destroy
+		@word = Word.find(params[:id])
+		@word.destroy
+		 
+		redirect_to words_path
+	end
+	private
+		def word_params
+	    	params.require(:word).permit(:word)
+    	end
 end
